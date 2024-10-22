65a4c4f2d0dd0a934912a0003fcbad19dc8c5cad
fairy <3475397231@qq.com>
2024-10-22 15:49:27 +0800

    【语音存储与回放】正在变基

--- Module/cpu_temperature.cpp
+++ Module/cpu_temperature.cpp
@@ -11,11 +11,11 @@
  * @brief 测量CPU温度
  * @details 返回值:温度值(扩大了100倍,单位:℃.)
  */
-auto thermometer::Get_Temperature() -> short {
-    uint32_t adcx = Get_Adc_Average(&hadc1, ADC_CHANNEL_16, 10);
-    double temperature = (double)adcx * (3.3 / 4096);
-    temperature = (temperature - 0.76) / 0.0025 + 25;
-    auto result = (short)(temperature * 100);
-    return result;
-}
+//auto thermometer::Get_Temperature() -> short {
+//    uint32_t adcx = Get_Adc_Average(&hadc1, ADC_CHANNEL_16, 10);
+//    double temperature = (double)adcx * (3.3 / 4096);
+//    temperature = (temperature - 0.76) / 0.0025 + 25;
+//    auto result = (short)(temperature * 100);
+//    return result;
+//}
 //#endif
