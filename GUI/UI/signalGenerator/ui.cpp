1547085562bfca3ac032f9e42672a7c086849a6c
fairy <3475397231@qq.com>
2024-10-22 16:45:13 +0800

    【双音频信号发生器】模拟器——搭建一个空白模板

---
+++ GUI/UI/signalGenerator/ui.cpp
@@ -0,0 +1,19 @@
+//
+// Created by fairy on 2024/10/22 16:34.
+//
+#include "ui.hpp"
+#include "GUI.hpp"
+#if 1
+// 头文件
+
+// 函数
+auto Screen::init() -> void {
+    Component::set_parent(gui->main.screen);
+
+}
+
+auto Events::init() -> void {
+
+
+}
+#endif
---
+++ GUI/UI/signalGenerator/ui.hpp
@@ -0,0 +1,24 @@
+//
+// Created by fairy on 2024/10/22 16:34.
+//
+#ifndef SIMULATOR_UI_HPP
+#define SIMULATOR_UI_HPP
+
+#include "Module_Conf.h"
+#include "lvgl.h"
+/* 预编译命令 */
+#if 1
+/*********************组件*******************/
+typedef struct
+{
+    struct
+    {
+        lv_obj_t *screen;// 屏幕自身
+
+    } main;// 主屏幕
+
+}lv_ui_t;
+
+/* 预编译命令 */
+#endif
+#endif //SIMULATOR_UI_HPP
