5838407f095c1c06444773b2a5cc3197ae879f54
fairy <3475397231@qq.com>
2024-11-04 14:22:54 +0800

    【双音频信号发生器】模拟器——准备优化线性插值函数

--- GUI/Component/WaveCurve.hpp
+++ GUI/Component/WaveCurve.hpp
@@ -89,8 +89,8 @@ private:
 
 
     // 定义步长，决定曲线的平滑度
-    static constexpr float smoothness = 0.1f;// 定为0.1比较适合，不推荐改，如果改的话，把下面的缓冲数组尺寸也改了1/smoothness+1，得是整数
-    static constexpr uint16_t buffer_size = 11;
+    static constexpr float smoothness = 0.1f;// 定为0.1比较适合，不推荐改，再小会非常卡，太大曲线会不连续
+    static constexpr uint16_t buffer_size = (uint16_t) (1.0f / smoothness) + 1;
 
     // 定义大量静态数组时千万不要把volatile去掉，程序对数组的优化，尤其是静态数组，总是容易优化死
     // 根据我的远古回忆，上次优化导致卡死还是在使用TI(默认-O2还是-O3来着)时，定义了一个很小的静态数组，结果排查了半天
@@ -121,72 +121,34 @@ auto WaveCurve::switch_ptr(T *&p1, T *&p2) -> void
 }
 
 
-template<typename Coord>
-auto WaveCurve::get_max_min(Coord value, Coord &max, Coord &min) -> void
-{
-    if (value < min)
-        min = value;
-    else if (value > max)
-        max = value;
-}
-
-// 显式实例化
-template auto WaveCurve::get_max_min(uint16_t value, uint16_t &max, uint16_t &min) -> void;
-
-
 // 获取一次绘制所需的点数
 template<WaveCurve::Type draw_type>
 constexpr auto WaveCurve::getOncePoints() -> uint16_t
 {
-    if constexpr (draw_type == Type::Interpolated_Line)
+    if constexpr (draw_type == Type::BezierCurve2)
+    {
+        return 3;// 三个点
+    } else if constexpr (draw_type == Type::BezierCurve3 || draw_type == Type::CatmullRomSp_line)
+    {
+        return 4; // 四个点
+    } else // 默认为 Type::Interpolated_Line线性插值算法
     {
         return 2; // 两个点
-    } else if constexpr (draw_type == Type::BezierCurve2)
-    {
-        return 3;// 三个点
-    } else if constexpr (draw_type == Type::BezierCurve3)
-    {
-        return 4; // 四个点
-    } else if constexpr (draw_type == Type::CatmullRomSp_line)
-    {
-        return 4; // 四个点
-    } else
-    {
-        return 3;// 三个点
     }
 }
 
-/**
- * @brief 通用清除函数
- * @tparam Coord
- * @tparam Color
- * @param x
- * @param y
- * @param color
- */
-template<WaveCurve::Type draw_type, typename Coord, typename Color>
-auto WaveCurve::clean_general(const Coord *x, const Coord *y, Color color) -> void
-{
-    constexpr auto once_points = getOncePoints<draw_type>();
-    Coord min_y = y[0], max_y = y[0];
-    for (int i = 1; i < once_points; ++i)
-    {
-        get_max_min<Coord>(y[i], max_y, min_y);
-    }
-    LCD_Color_Clean(x[0], min_y, x[1], max_y, color);
-}
 
 /*DrawFunction特化版本*/
 // 插值画线
 template<typename Coord, typename Color>
 struct WaveCurve::DrawFunction<WaveCurve::Type::Interpolated_Line, Coord, Color>
 {
-    static auto inline draw(const Coord *x, const Coord *y, Color color) -> void
+    static auto inline draw(const Coord x[], const Coord y[], Color color) -> void
     {
         draw_interpolated_line(x, y, color);
     }
 
-    static auto inline clean(const Coord *x, const Coord *y, Color color) -> void
+    static auto inline clean(const Coord x[], const Coord y[], Color color) -> void
     {
         draw_interpolated_line(x, y, color);// 这样反而更快
     }
@@ -199,7 +161,7 @@ struct WaveCurve::DrawFunction<WaveCurve::Type::BezierCurve2, Coord, Color>
     static auto inline draw(int &i, Coord &N, const Coord x[], const Coord y[], Color &bg_color, Color &color) -> void
     {
         uint16_t end_index = N - getOncePoints<WaveCurve::Type::BezierCurve2>();
-        float t=0.0f;
+        float t = 0.0f;
         for (int j = 0; j <= 10; ++j)
         {
             float one_minus_t = 1.0f - t;
@@ -215,7 +177,7 @@ struct WaveCurve::DrawFunction<WaveCurve::Type::BezierCurve2, Coord, Color>
                 LCD_Set_Pixel(pBuff_x[j], pCleanBuff_y[j], bg_color);// 清除旧像素点
                 LCD_Set_Pixel(pBuff_x[j], pDrawBuff_y[j], color);// 绘制新像素点
             }
-            t+=smoothness;
+            t += smoothness;
         }
         switch_ptr(pCleanBuff_y, pDrawBuff_y);// 交换指针，把pBuff_y当做下一轮pBuff_new_y
     }
@@ -228,7 +190,7 @@ struct WaveCurve::DrawFunction<WaveCurve::Type::BezierCurve3, Coord, Color>
     static auto inline draw(int &i, Coord &N, const Coord x[], const Coord y[], Color &bg_color, Color &color) -> void
     {
         uint16_t end_index = N - getOncePoints<WaveCurve::Type::BezierCurve3>();
-        float t=0.0f;
+        float t = 0.0f;
         for (int j = 0; j <= 10; ++j)
         {
             float one_minus_t = 1.0f - t;
@@ -252,7 +214,7 @@ struct WaveCurve::DrawFunction<WaveCurve::Type::BezierCurve3, Coord, Color>
                 LCD_Set_Pixel(pBuff_x[j], pCleanBuff_y[j], bg_color);// 清除旧像素点
                 LCD_Set_Pixel(pBuff_x[j], pDrawBuff_y[j], color);// 绘制新像素点
             }
-            t+=smoothness;
+            t += smoothness;
         }
         switch_ptr(pCleanBuff_y, pDrawBuff_y);// 交换指针，把pBuff_y当做下一轮pBuff_new_y
 
@@ -291,27 +253,6 @@ struct WaveCurve::DrawFunction<WaveCurve::Type::CatmullRomSp_line, Coord, Color>
         }
         switch_ptr(pCleanBuff_y, pDrawBuff_y);// 交换指针，把pBuff_y当做下一轮pBuff_new_y
     }
-
-    static auto inline clean(const Coord *x, const Coord *y, Color color) -> void
-    {
-        float t = 0.0;
-        while (t <= 1.0)
-        {
-            float t2 = t * t;
-            float t3 = t2 * t;
-            // 转换浮点数坐标到整数
-            auto px = (uint16_t) (0.5 *
-                                  (2 * x[1] + (-x[0] + x[1]) * t + (2 * x[0] - 5 * x[1] + 4 * x[2] - x[3]) * t2 +
-                                   (-x[0] + 3 * x[1] - 3 * x[2] + x[3]) * t3));
-            auto py = (uint16_t) (0.5 * (2 * y[1] +
-                                         (-y[0] + y[2]) * t +
-                                         (2 * y[0] - 5 * y[1] + 4 * y[2] - y[3]) * t2 +
-                                         (-y[0] + 3 * y[1] - 3 * y[2] + y[3]) * t3));
-            // 设置像素点
-            LCD_Set_Pixel(px, py, color);
-            t += smoothness;
-        }
-    }
 };
 
 /**
@@ -340,9 +281,11 @@ auto WaveCurve::draw_curve(Data data[], Coord N, Data value, Coord Start_x, Coor
     // 如果数据点过少，则无法画线
     if (N < once_points) return;
 
-    Coord x[once_points], y[once_points + 1];
+    Coord x[once_points], y[once_points];
     float ratio = (Height - 1.0f) / Max_Value;
     float step = Width / (N - 1.0f);// 步长
+
+    auto y_old=(Coord) (Start_y + Height - value * ratio);
 
     // 一放在前面就会卡死，不知缘由
 //        data[N - 1] = value;
@@ -362,9 +305,55 @@ auto WaveCurve::draw_curve(Data data[], Coord N, Data value, Coord Start_x, Coor
         if constexpr (draw_type == Type::Interpolated_Line)
         {
             // 确认新点
-            y[once_points] = (Coord) (Start_y + Height - data[i + once_points] * ratio);
-            DrawFunction<draw_type, Coord, Color>::clean(x, y, bg_color);// 清除旧曲线
-            DrawFunction<draw_type, Coord, Color>::draw(x, y + 1, color);// 绘制新曲线
+
+            // Bresenham's line algorithm
+            Coord x0 = x[0], y0 = y[0], x1 = x[1], y1 = y[1];
+            int dx = (abs(x1 - x0)), sx = x0 < x1 ? 1 : -1;// sx：决定递增方向
+            int dy = (-abs(y1 - y0)), sy = y0 < y1 ? 1 : -1;
+            int err = (dx + dy), e2;
+
+            for (;;)
+            {
+                LCD_Set_Pixel(x0, y0, bg_color);
+                if (x0 == x1 && y0 == y1) break;
+                e2 = 2 * err;
+                if (e2 >= dy)
+                {
+                    err += dy;
+                    x0 += sx;
+                }
+                if (e2 <= dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+       // 重置
+             x0 = x[0];
+            y0 = y1;
+            x1 = x[1];
+            y1 = y_old;
+             dx = (abs(x1 - x0)), sx = x0 < x1 ? 1 : -1;// sx：决定递增方向
+             dy = (-abs(y1 - y0)), sy = y0 < y1 ? 1 : -1;
+             err = (dx + dy), e2;
+
+            for (;;)
+            {
+                LCD_Set_Pixel(x0, y0, color);
+                if (x0 == x1 && y0 == y1) break;
+                e2 = 2 * err;
+                if (e2 >= dy)
+                {
+                    err += dy;
+                    x0 += sx;
+                }
+                if (e2 <= dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+            y_old=y[1];
         } else
         {
             DrawFunction<draw_type, Coord, Color>::draw(i, N, x, y, bg_color, color);// 绘制新曲线
@@ -402,88 +391,6 @@ auto WaveCurve::draw_interpolated_line(const Coord *x, const Coord *y, Color col
             err += dx;
             y0 += sy;
         }
-    }
-}
-
-/**
- * @brief 绘制二次贝塞尔曲线
- * @note 需要把模板参数里的once_points参数设置为3，因为二次贝塞尔曲线需要三个控制点
- * @param x
- * @param y
- * @param color
- */
-template<typename Coord, typename Color>
-auto WaveCurve::draw_BezierCurve2(const Coord *x, const Coord *y, Color color) -> void
-{
-    float t = 0.0f;
-    Coord px, py;
-    while (t <= 1.0f)
-    {
-        float t2 = t * t;
-        // 转换浮点数坐标到整数
-        px = (Coord) ((1 - t) * (1 - t) * x[0] + 2 * (1 - t) * t * x[1] + t2 * x[2]);
-        py = (Coord) ((1 - t) * (1 - t) * y[0] + 2 * (1 - t) * t * y[1] + t2 * y[2]);
-        // 设置像素点
-        LCD_Set_Pixel(px, py, color);
-        t += smoothness;
-    }
-}
-
-/**
- * @brief 绘制三次贝塞尔曲线
-* @note 需要把模板参数里的once_points参数设置为3，因为三次贝塞尔曲线需要四个控制点
- * @param x
- * @param y
- * @param color
- */
-template<typename Coord, typename Color>
-auto WaveCurve::draw_BezierCurve3(const Coord *x, const Coord *y, Color color) -> void
-{
-    Coord px, py;
-    float t = 0.0f;
-    while (t <= 1.0f)
-    {
-        float t2 = t * t;
-        float t3 = t2 * t;
-        // 转换浮点数坐标到整数
-
-        px = (Coord) ((1 - t) * (1 - t) * (1 - t) * x[0] + 3 * (1 - t) * (1 - t) * t * x[1] +
-                      3 * (1 - t) * t2 * x[2] + t3 * x[3]);
-        py = (Coord) ((1 - t) * (1 - t) * (1 - t) * y[0] + 3 * (1 - t) * (1 - t) * t * y[1] +
-                      3 * (1 - t) * t2 * y[2] + t3 * y[3]);
-
-        // 设置像素点
-        LCD_Set_Pixel(px, py, color);
-        t += smoothness;
-    }
-}
-
-/**
- * @brief 绘制 Catmull-Rom 样条曲线
- * @note 需要把模板参数里的once_points参数设置为4，因为 Catmull-Rom 样条曲线需要四个控制点
- * @param x
- * @param y
- * @param color
- */
-template<typename Data, typename Color>
-auto WaveCurve::draw_CatmullRomSp_line(const Data *x, const Data *y, Color color) -> void
-{
-    float t = 0.0;
-    while (t <= 1.0)
-    {
-        float t2 = t * t;
-        float t3 = t2 * t;
-        // 转换浮点数坐标到整数
-        auto px = (uint16_t) (0.5 *
-                              (2 * x[1] + (-x[0] + x[1]) * t + (2 * x[0] - 5 * x[1] + 4 * x[2] - x[3]) * t2 +
-                               (-x[0] + 3 * x[1] - 3 * x[2] + x[3]) * t3));
-        auto py = (uint16_t) (0.5 * (2 * y[1] +
-                                     (-y[0] + y[2]) * t +
-                                     (2 * y[0] - 5 * y[1] + 4 * y[2] - y[3]) * t2 +
-                                     (-y[0] + 3 * y[1] - 3 * y[2] + y[3]) * t3));
-        // 设置像素点
-        LCD_Set_Pixel(px, py, color);
-        t += smoothness;
     }
 }
 
--- GUI/UI/signalGenerator/ui.cpp
+++ GUI/UI/signalGenerator/ui.cpp
@@ -201,7 +201,7 @@ public:
         int wave_date = sine_wave[wave_index] + bias;
         wave_date = (wave_date > max_value) ? max_value : (wave_date < 0) ? 0 : wave_date;
 
-        WaveCurve::draw_curve<WaveCurve::Type::BezierCurve2, uint8_t>(Buf,point_cnt, static_cast<uint8_t>(wave_date),
+        WaveCurve::draw_curve<WaveCurve::Type::Interpolated_Line, uint8_t>(Buf,point_cnt, static_cast<uint8_t>(wave_date),
                                                                                 start_x,
                                                                                 start_y, chart_width, chart_height, 255,
                                                                                 0xFFFF, 0);
