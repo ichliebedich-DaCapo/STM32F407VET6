79df04db3ca38dbdbacf624e670addfefac9a893
DV-WU-158 <2357949763@qq.com>
2024-11-17 14:15:54 +0800

    【直线位移传感器】ADC配置完毕

--- Application/LinearDisplacementSensor/app.cpp
+++ Application/LinearDisplacementSensor/app.cpp
@@ -9,6 +9,8 @@
 #include "adc.h"
 #include "key.hpp"
 
+#define BUFFER_SIZE 100
+float adcValues[BUFFER_SIZE]{};
 
 void app_init()
 {
@@ -40,5 +42,14 @@ void key_handler()
 // 用于采集ADC数据
 void adc1_isr()
 {
-    HAL_ADC_GetValue(&hadc1);
+    static uint16_t index = 0;
+    // 获取ADC值
+    uint16_t adcValue = HAL_ADC_GetValue(&hadc1);
+
+    // 将ADC值存入数组
+    adcValues[index] = adcValue/4095.0f*200;
+
+    // 更新索引，循环使用数组
+    index = (index + 1) % BUFFER_SIZE;
+
 }
