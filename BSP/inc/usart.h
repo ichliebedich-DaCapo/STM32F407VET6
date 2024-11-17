15912b565816751a25e76dab95011bc292ac9aac
DV-WU-158 <2357949763@qq.com>
2024-11-17 16:08:21 +0800

    【直线位移传感器】usart重定向完成

---
+++ BSP/inc/usart.h
@@ -0,0 +1,26 @@
+//
+// Created by DV on 2024/11/17.
+//
+
+#ifndef FURINA_USART_H
+#define FURINA_USART_H
+
+#include "JYZQ_Conf.h"
+/* 预编译命令 */
+#if 1
+// 头文件
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// 宏定义
+
+
+// 接口
+
+#ifdef __cplusplus
+}
+#endif
+/* 预编译命令 */
+#endif
+#endif //FURINA_USART_H
---
+++ BSP/src/usart.c
@@ -0,0 +1,58 @@
+//
+// Created by DV on 2024/11/17.
+//
+
+#include <stdio.h>
+#include "usart.h"
+
+#if 1
+// 头文件
+#include "stm32f4xx_hal.h"
+
+// 变量
+UART_HandleTypeDef huart1;
+
+
+// 函数
+void usart1_init() {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    __HAL_RCC_USART1_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    HAL_UART_Init(&huart1);
+
+}
+
+
+int fputc(int ch, FILE *f)
+
+{
+
+      HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xffff);
+//    while( __HAL_UART_GET_FLAG(&huart1, UART_FLAG_TXE)==RESET);
+//    huart1.Instance->DR=ch;
+
+  return ch;
+
+}
+#endif
