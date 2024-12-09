bfd2708cb34b7feec47684616fd9fcebb4fc5d92
DV-WU-158 <2357949763@qq.com>
2024-12-09 18:42:27 +0800

    【直线位移传感器】计时器频率改成200hz

--- Application/LinearDisplacementSensor/app.cpp
+++ Application/LinearDisplacementSensor/app.cpp
@@ -18,7 +18,7 @@ uint8_t rxBuffer[1];
 void app_init()
 {
    adc1_init();
-   timer2_init(FREQ_84M_to_50);
+   timer2_init(FREQ_84M_to_200);
    usart1_init();
    HAL_UART_Receive_IT(&huart1, rxBuffer, 1);
 }
@@ -66,9 +66,9 @@ void adc1_isr() {
     // 获取ADC值
     uint16_t adcValue = HAL_ADC_GetValue(&hadc1);
     // 打印ADC值
-    printf("%f\r\n", (4095-adcValue) / 4095.0f * 3.3 * 5 / 3.3);//方案1：传输给单片机处理 真实电压*电压密度=长度
+    //printf("%f\r\n", (4095-adcValue) / 4095.0f * 3.3 * 5 / 3.3);//方案1：传输给单片机处理 真实电压*电压密度=长度
 //    printf("%d\r\n", adcValue);//方案2：传输给上位机处理
-
+    printf("%f\r\n", adcValue / 4095.0f * 3.3);
 }
 
 /*实现串口的接收完成中断回调函数*/
