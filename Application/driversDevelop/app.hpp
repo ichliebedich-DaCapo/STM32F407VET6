2b91fda756dc7e5b90e82363ec84b1a4da844baa
fairy <3475397231@qq.com>
2025-01-09 13:54:22 +0800

    【驱动开发】准备重构编译宏

--- Application/driversDevelop/app.hpp
+++ Application/driversDevelop/app.hpp
@@ -4,22 +4,8 @@
 #ifndef FURINA_APP_HPP
 #define FURINA_APP_HPP
 
-#include "JYZQ_Conf.h"
 
-#define USE_ADC
-#define USE_TIMER
-#define APP_NO_RTOS
-#define USE_DAC // 使用DAC
-#define USE_PLAYER  // 使用播放器
-#define USE_FLASH_STORAGE // 使用Flash存储
-#define USE_WAVE_SIGNAL // 使用波形信号
+// 该宏还是有着自己的作用，比如控制FreeRTOS和GUI的使用
 
-/**************************板级**************************/
-#define USE_SPI  // 使用SPI
-#define USE_SPI_FLASH  // 使用SPI Flash
-#define USE_TIMER // 使用定时器
-#define USE_ADC  // 使用ADC
-#define USE_DAC // 使用DAC
-#define USE_USART  // 使用UART
 
 #endif //FURINA_APP_HPP
