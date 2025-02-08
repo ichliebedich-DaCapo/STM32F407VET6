02dc908eb6bbc8df8bab9e7e49277e1fd030038b
fairy <3475397231@qq.com>
2025-02-08 14:06:44 +0800

    【工程重构】整理并优化了模拟器的CMakeLists

--- Adapter/GUI/lvgl-simulator/common_functions.cmake
+++ Adapter/GUI/lvgl-simulator/common_functions.cmake
@@ -1,3 +1,9 @@
+# -------------------工具函数-------------------
+# 打印信息
+function(print info)
+    message(STATUS "-->[Info]:${info}")
+endfunction()
+
 # 定义查找源文件的函数
 function(find_sources variable)
     set(files)
@@ -8,9 +14,11 @@ function(find_sources variable)
     set(${variable} ${files} PARENT_SCOPE)
 endfunction()
 
+# ------------------配置项-----------------
+# 变量定义
+set(STATIC_LIB_LD OFF CACHE INTERNAL "Independent static library link option")
 
-
-# 打印信息
-function(print info)
-    message(STATUS "-->[Info]:${info}")
-endfunction()
+# 开启静态库链接
+macro(options_enable_static_lib_ld)
+    set(STATIC_LIB_LD ON CACHE INTERNAL "Independent static library link option")
+endmacro()
